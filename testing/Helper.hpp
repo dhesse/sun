e95e375a533f9f334c58806f33021331be8b6306
Dirk Hesse <herr.dirk.hesse@gmail.com>
2013-02-28 17:31:42 +0000

    Adding tests for associativity and binary '*'

--- testing/Helper.hpp
+++ testing/Helper.hpp
@@ -7,15 +7,21 @@ using namespace sun;
 template <int N> SU<N> rsu(){
   SU<N> A;
   for (int n =0; n < N*N; ++n)
-    A(n) = typename SU<N>::data_t(rand(), rand());
+    A(n) = typename SU<N>::data_t(1. - 2.*rand()/RAND_MAX,
+				  1. - .2* rand()/RAND_MAX);
   return A;
 }
 
-template <int N> bool almost_equal(const SU<N>& A, const SU<N>& B){
+template <int N> bool almost_equal(const SU<N>& A, const SU<N>& B, int k = 5){
   bool result = true;
   for (int n =0; n < N*N && result; ++n)
-    if (abs( A(n) - B(n) ) > std::numeric_limits<double>::epsilon()*5)
+    if (abs( A(n) - B(n)) / abs(A(n) + B(n)) >
+	(std::numeric_limits<double>::epsilon() * k)){
+      std::cout << A(n).real() << ", " << A(n).imag() << std::endl;
+      std::cout << abs( A(n) - B(n) ) << std::endl;
+      std::cout << std::numeric_limits<double>::epsilon() * k << std::endl;
       result = false;
+    }
   return result;
 }
 
@@ -26,4 +32,3 @@ template <int N> bool equal(const SU<N>& A, const SU<N>& B){
   return result;
 }
 #endif
-
